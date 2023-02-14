383c6c3a9c61e1ea2ffb42033f82a91a43b095c9
Maria <araujomaria84@gmail.com>
2023-02-14 20:56:34 +0100

    Add function userMsj

--- js/main.js
+++ js/main.js
@@ -13,9 +13,37 @@
 //Comparar el número escrito cn el input aleatorio y pintar las pistas en la pantalla
 //Actualizar el contador de intentos por cada prueba.
 
-const btn = document.querySelector('.js__button');
 const inpNumber = document.querySelector('.js__number');
-const inpclue = document.querySelector('.js__clue');
-const attempsNum = document.querySelector('.js__attemps')
+const btn = document.querySelector('.js__button');
+const attempsNum = document.querySelector('.js__attemps');
+const msj = document.querySelector('.js__msj');
 
+function userMsj(){
+    const inpNumValue = parseInt(inpNumber.value);
+    let msjResult = "Tienes que introducir un numero";
+    if (inpNumValue === randomNumber){
+    console.log('Has ganado campeona!!!');
+        msjResult = 'Has ganado campeona';
+    } else if (inpNumValue < randomNumber){
+        console.log('Demasiado bajo');
+        msjResult = "Demasiado bajo";
+    } else if (inpNumValue > randomNumber){
+        console.log('Demasiado alto');
+        msjResult = "Demasiado alto";
+    }
+    msj.innerHTML = msjResult;
+}
 
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+let randomNumber = getRandomNumber(100);
+console.log('Mi número aleatorio es: ' + randomNumber);
+
+function handleClick(event){
+    event.preventDefault();
+
+    userMsj()
+}
+
+btn.addEventListener('click' , handleClick);
